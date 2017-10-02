017569513f58a72d0798ef9d993c2ab59d0c9a75
stlaumade <stlaumade@gs161b.AD.ECS.VUW.AC.NZ>
2017-10-03 11:34:55 +1300

    Testing class
    Added template to fill in for testing later on

---
+++ src/Testing/testing.java
@@ -0,0 +1,33 @@
+package Testing;
+
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+
+public class testing {
+
+	/**
+	 * @author Madeleine St-Laurent-Guerin
+	 */
+
+	@Test
+	public void firstTest() {
+
+		//Insert test code & assert statement
+
+	}
+
+	@Test
+	public void secondTest() {
+
+		//Insert test code & assert statement
+
+	}
+
+
+}
+
