cf8e2c51367b11cadada99d66c438cd8630ea4e9
stlaumade <stlaumade@gs161b.AD.ECS.VUW.AC.NZ>
2017-10-05 11:54:30 +1300

    Test

--- src/Testing/Testing.java
+++ src/Testing/Testing.java
@@ -1,6 +1,15 @@
 package Testing;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.junit.Test;
 
@@ -8,6 +17,8 @@ import Controller.Backend_BurgerCoApp;
 import Controller.BadPasswordException;
 import Controller.Employee;
 import Controller.NonExistentUserException;
+import Controller.Order;
+import Database.LocalSQLiteDB;
 
 public class Testing {
 
@@ -30,6 +41,7 @@ public class Testing {
 			app.verifyUserCredentials(employee_id, password_input);
 			employee = Employee.getUserDB(employee_id);
 			answer = true;
+				
 		} catch (BadPasswordException e) {
 			System.out.println("User credentials could not be verified");
 			return;
@@ -43,8 +55,60 @@ public class Testing {
 	}
 
 	@Test
-	public void test2(){
+	public void testGetOrderID(){
+		
+		int orderid = -1;
 
+        File database_file = new File("Burger.sqlite");
+        LocalSQLiteDB db = new LocalSQLiteDB("sqlite", database_file.getAbsolutePath());
+        
+        try (Connection c = db.connection()) {
+            try (PreparedStatement stmt = c.prepareStatement("SELECT order_id FROM Burger_Order WHERE user_alias = ?")) {
+                stmt.setString(1, "Test1");
+              try (ResultSet r = stmt.executeQuery()) {
+                    while (r.next()) {
+                        orderid = r.getInt("order_id");   
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assertThat(orderid, is(1));
+	}
+	
+	@Test
+	public void testStockLevel(){
+
+		int stocklevel = -1;
+		
+		File database_file = new File("Burger.sqlite");
+		LocalSQLiteDB db = new LocalSQLiteDB("sqlite", database_file.getAbsolutePath());
+		try (Connection c = db.connection()) {
+			try (PreparedStatement stmt = c.prepareStatement("SELECT i.quantity FROM Ingredient as i WHERE ingredient_name = ?;")) {
+				stmt.setString(1, "Beetroot");
+
+				try (ResultSet r = stmt.executeQuery()) {
+					while (r.next()) {
+						stocklevel = r.getInt("quantity");
+					}
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		 assertThat(stocklevel, is(4));
 	}
 
 }
